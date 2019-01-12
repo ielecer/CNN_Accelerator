e00ec3d06cbd55be36ae5bd19f64cda7704c2b13
ielecer <myj223476@gmail.com>
2019-01-12 20:09:37 +0800

    Create directory Deep_learning_from_Scratch

---
+++ Deep_Learning_from_Scratch/ch02/or_gate.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def OR(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.2
+	tmp = np.sum(w * x) + b
+	if tmp <= 0:
+		return 0
+	else:
+		return 1
---
+++ Deep_Learning_from_Scratch/ch02/xor_gate.py
@@ -0,0 +1,9 @@
+from nand_gate import NAND
+from or_gate import OR
+from and_gate import AND
+
+def XOR(x1, x2):
+	s1 = NAND(x1, x2)
+	s2 = OR(x1, x2)
+	y = AND(s1, s2)
+	return y
