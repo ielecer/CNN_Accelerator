aa79e91ca7b8ecc9515f56e6fba355a22c033aab
ielecer <myj223476@gmail.com>
2019-01-13 14:38:02 +0800

    Add batch processing into forward propogation

--- Deep_Learning_from_Scratch/Ch03_Neural_Network/mnist_forward.py
+++ Deep_Learning_from_Scratch/Ch03_Neural_Network/mnist_forward.py
@@ -47,11 +47,26 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+# # Check the shape of input data and weights
+# W1, W2, W3 = network['W1'], network['W2'], network['W3']
+# print(x.shape)
+# print(len(x))
+# print(x[0].shape)
+# print(W1.shape)
+# print(W2.shape)
+# print(W3.shape)
+
+batch_size = 100		# batch processing
 accuracy_cnt = 0
-for i in range(len(x)):
-	y = predict(network, x[i])
-	p = np.argmax(y)
-	if p == t[i]:
-		accuracy_cnt += 1
+for i in range(0, len(x), batch_size):
+	x_batch = x[i:i+batch_size]
+	y_batch = predict(network, x_batch)
+	# axis=0: 0th dimension indicates column direction.
+	# axis=1: 1st dimension indicates row direction.
+	# np.argmax: access the index of maximum value for the whole row direction.
+	p = np.argmax(y_batch, axis=1)
+	# Compare 100 results with 100 tags to get 100 boolean values and
+	# then sum them all.
+	accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
 print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
