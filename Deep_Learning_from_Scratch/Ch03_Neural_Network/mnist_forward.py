d5e15320bde1fffc1bb368d2f874108f2761462f
ielecer <myj223476@gmail.com>
2019-01-13 13:54:40 +0800

    Succeed to build the forward propogation of neural network

--- Deep_Learning_from_Scratch/Ch03_Neural_Network/mnist_forward.py
+++ Deep_Learning_from_Scratch/Ch03_Neural_Network/mnist_forward.py
@@ -1,29 +1,57 @@
+# coding: utf-8
 import sys, os
 sys.path.append(os.pardir)		# Add parent directory into system path.
 from dataset.mnist import load_mnist
 import numpy as np
-from PIL import Image
+import pickle
+from PIL import Image 		# PIL: Python Image Library
 
 def img_show(img):
 	pil_img = Image.fromarray(np.uint8(img))
 	pil_img.show()
 
+def softmax(a):
+	c = np.max(a)
+	exp_a = np.exp(a - c)		# avoid overflow when a is large enough
+	sum_exp_a = np.sum(exp_a)
+	y = exp_a / sum_exp_a
+	return y
+
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
 # It costs few minutes for the first loading.
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten = True,
-	normalize = False)
+def get_data():
+	(x_train, t_train), (x_test, t_test) = \
+	load_mnist(normalize=True, flatten=True, one_hot_label=False)
+	return x_test, t_test  # Data used to test.
 
-# Print shape for each data.
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+# Load parameters which come from learning phase.
+def init_network():
+	with open("sample_weight.pkl", 'rb') as f:
+		network = pickle.load(f)
+	return network
 
-img = x_train[0]
-label = t_train[0]
-print(label)
+def predict(network, x):
+	W1, W2, W3 = network['W1'], network['W2'], network['W3']
+	b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-print(img.shape)
-img = img.reshape(28, 28)
-print(img.shape)
+	a1 = np.dot(x, W1) + b1
+	z1 = sigmoid(a1)
+	a2 = np.dot(z1, W2) + b2
+	z2 = sigmoid(a2)
+	a3 = np.dot(z2, W3) + b3
+	y = softmax(a3)
+	return y
 
-img_show(img)
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+	y = predict(network, x[i])
+	p = np.argmax(y)
+	if p == t[i]:
+		accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
