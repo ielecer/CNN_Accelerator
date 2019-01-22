d38627caeea4620356c6929ef65ab4895a395f6e
ielecer <myj223476@gmail.com>
2019-01-22 10:40:57 +0800

    Combine array tranformation into matrix multiplication

--- C/matmul.h
+++ C/matmul.h
@@ -1,5 +1,4 @@
-#define COLUMN 3
-
-void mat_trans(int *data_in, int (*data_out)[COLUMN], char row, char column);
 void matmul_2D_0(void);
+
+void mat_trans(int *data_in, int (*data_out)[2], char row, char column);
 void matmul_2D_1(void);
